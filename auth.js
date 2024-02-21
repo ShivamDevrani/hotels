6d7e8961ea663022edb507438a8e7b46cea6d894
ShivamDevrani <devranishivam1121@gmail.com>
2024-02-21 20:00:12 +0530

    added salting and hashing

--- auth.js
+++ auth.js
@@ -5,24 +5,24 @@ const LocalStrategy=require('passport-local').Strategy // also known as id passw
 
 const Person=require('./models/Person');
 
+
 // defining middleware for authentication local strategy which will verify
 passport.use(new LocalStrategy(async (username,password,done)=>{
     //authentication logic here
     try{ 
-         console.log('recieved credentials:',username,password);
+
          // finding the username present in person model or not
          const user= await Person.findOne({username:username});
          
         
          if(!user)
            return done(null, false, { message: 'Incorrect username.' });
-   
-         if(user.password!=password)
-         {
-             return done(null,false,{message:'incorrect password'});
-         }
-         
+
+         const ispasswordmatch=user.comparePassword(password);
+         if(ispasswordmatch)
         return done(null,user);
+      else
+      return done(null,false,{message:"incorrect password"});
   
     }catch(err){
          console.log(err);
--- models/Person.js
+++ models/Person.js
@@ -1,5 +1,8 @@
 const mongoose=require('mongoose');
 
+// for generating hashed password using salting and hashing process
+const bcrypt=require('bcrypt');
+
 //define a person schema
 const personSchema=new mongoose.Schema({
     name:{
@@ -44,6 +47,50 @@ const personSchema=new mongoose.Schema({
 
 
 })
+
+//pre is a middle ware which will convert the password into hashed string and then save
+//not we do not prefer to use arrow function in pre middleware because this doesnt have meaning in arrow function
+personSchema.pre('save',async function(next){
+
+    //person is a reference to the document being saved. and this refers to document (or instance of schema) on which the middleware
+    //is currently working .
+    const person=this;
+
+    // we will hash the password only if person changes the password or is new login
+
+    if(!person.isModified('password')) return next();
+
+    try{
+        //generating salt randomly
+    
+        const salt=await bcrypt.genSalt(10);
+        //hashed password
+        const hashpassword=await bcrypt.hash(person.password,salt);
+
+        //overiding the plain password with hashedpassword
+        person.password=hashpassword;
+        //next callback function is used to give the signal to mongoose that now you
+        //  can move to either next middleware or just save this progtam
+        next();
+
+    }catch(err)
+    {
+       return   next(err);
+    }
+})
+
+//making function inside personschema
+personSchema.methods.comparePassword=async function(candidatePassword){
+    
+    try{
+          const ismatch=await bcrypt.compare(candidatePassword,this.password);
+          return ismatch;
+    }
+    catch(err){
+          throw err;
+    }
+
+}
 
 //creating persons model
 const Person=mongoose.model('Person',personSchema);
